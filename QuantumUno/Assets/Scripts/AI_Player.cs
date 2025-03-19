fbcdeab2a582db7efe0b240e3773f3dad67cf6ba
lucbruno234 <lucbruno@umich.edu>
2025-03-19 19:44:28 -0400

    ai player bug fix

--- QuantumUno/Assets/Scripts/AI_Player.cs
+++ QuantumUno/Assets/Scripts/AI_Player.cs
@@ -4,37 +4,24 @@ using UnityEngine;
 
 public class AI_Player : Player_Base
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public IEnumerator TakeTurn(ref List<GameObject> deck, ref List<GameObject> discard_pile, ref int turnOrder)
+    public IEnumerator TakeTurn()
     {
         Debug.Log("AI is thinking...");
         yield return new WaitForSeconds(1.0f); // Simulate thinking time
 
-        // Access the Card component of the top card
-        Card topCardComponent = (Card)topCard.GetComponent(typeof(Card));
+        // Access the top card from GameManager
+        GameObject topCard = GameManager.Instance.discard_pile[GameManager.Instance.discard_pile.Count - 1];
+        Card topCardComponent = topCard.GetComponent<Card>(); // Get the Card component
         bool cardPlayed = false;
 
         // Check for a valid card to play
-        foreach (GameObject cardObject in hand)
+        foreach (Card cardObject in hand)
         {
-            Card card = (Card)cardObject.GetComponent(typeof(Card));
+            Card card = cardObject.GetComponent<Card>(); // Get the Card component
+            GameObject cardGameObject = card.gameObject;
             if (card.color.Contains(topCardComponent.color[0]) || card.number.Contains(topCardComponent.number[0]))
             {
-                PlayCard(cardObject);
+                PlayCard(cardGameObject); // Pass the GameObject, not the Card component
                 cardPlayed = true;
                 break;
             }
@@ -43,7 +30,7 @@ public class AI_Player : Player_Base
         // If no valid card, draw a card
         if (!cardPlayed)
         {
-            DrawCard(deck);
+            DrawCard(GameManager.Instance.deck);
         }
 
         Debug.Log("AI's turn is over.");
@@ -51,33 +38,21 @@ public class AI_Player : Player_Base
 
     private void PlayCard(GameObject cardObject)
     {
-        Card card = (Card)cardObject.GetComponent(typeof(Card));
-        card.Play();
-        hand.Remove(cardObject);
-        GameManager.Instance.discard_pile.Add(cardObject);
+        Card card = cardObject.GetComponent<Card>(); // Get the Card component
+        card.Play(ref GameManager.Instance.deck, ref GameManager.Instance.discard_pile, ref GameManager.Instance.turn_order);
+        hand.Remove(card);
+        GameManager.Instance.UpdateTopCard(cardObject); // Update top card in GameManager
 
         Debug.Log($"AI played a card: {card.color[0]} {card.number[0]}");
 
         // Handle special cards
-        if (card is WildCard)
-        {
-            string chosenColor = ChooseColor();
-            Debug.Log($"AI chose {chosenColor} as the new color.");
-
-            //update the top cards color to the chosenColor by the player
-            GameObject topCard = GameManager.Instance.discard_pile[GameManager.Instance.discard_pile.Count - 1];
-            Card topCardComponent = (Card)topCard.GetComponent(typeof(Card));
-            topCardComponent.color[0] = chosenColor; 
-        }
-        else if (card is ReverseCard)
+        if (card is ReverseCard)
         {
             GameManager.Instance.ReverseTurnOrder();
-            Debug.Log("AI reversed the turn order.");
         }
         else if (card is SkipCard)
         {
             GameManager.Instance.SkipNextPlayer();
-            Debug.Log("AI skipped the next player.");
         }
     }
 
@@ -86,8 +61,9 @@ public class AI_Player : Player_Base
         if (deck.Count > 0)
         {
             GameObject drawnCard = deck[0];
+            Card currentCard = drawnCard.GetComponent<Card>();
             deck.RemoveAt(0);
-            hand.Add(drawnCard);
+            hand.Add(currentCard);
             Debug.Log("AI drew a card.");
         }
         else
@@ -95,38 +71,5 @@ public class AI_Player : Player_Base
             Debug.Log("No cards left in the deck.");
         }
     }
-
-    private string ChooseColor()
-    {
-        Dictionary<string, int> colorCounts = new Dictionary<string, int>();
-        foreach (GameObject cardObject in hand)
-        {
-            Card card = (Card)cardObject.GetComponent(typeof(Card));
-            if (card.color.Count > 0)
-            {
-                string color = card.color[0];
-                if (colorCounts.ContainsKey(color))
-                {
-                    colorCounts[color]++;
-                }
-                else
-                {
-                    colorCounts[color] = 1;
-                }
-            }
-        }
-
-        string chosenColor = "red";
-        int maxCount = 0;
-        foreach (var pair in colorCounts)
-        {
-            if (pair.Value > maxCount)
-            {
-                chosenColor = pair.Key;
-                maxCount = pair.Value;
-            }
-        }
-
-        return chosenColor;
-    }
+    
 }
