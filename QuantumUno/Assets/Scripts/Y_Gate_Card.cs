1284a219d169ee9618c598a5f23dcaf494a1d0fa
Ashley Sawicke <asawicke@umich.edu>
2025-03-19 19:27:49 -0400

    y gate card update - doesn't handle superposition

--- QuantumUno/Assets/Scripts/Y_Gate_Card.cs
+++ QuantumUno/Assets/Scripts/Y_Gate_Card.cs
@@ -2,14 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 //[CreateAssetMenu(fileName = "NewyGateCard", menuName = "Cards/Y Gate Card")]
+
+// take top card off of the discard pile, get the card object and look at the color
+// following Y gate behavior in QM
 public class Y_Gate_Card : Card
 {
     public override void Play(ref List<GameObject> deck, ref List<GameObject> discard_pile, ref int turnOrder)
     {
-        if (turnOrder > 1 || turnOrder < -1)
-            turnOrder /= 2;
-      
+        if (turnOrder > 1 || turnOrder < -1) turnOrder /= 2;
+
+        GameObject topCard = discard_pile[discard_pile.Count - 1];
+        Card cardComponent = topCard.GetComponent<cardComponent>();
+        
+        if(cardComponent != null && cardComponent.color[0]){
+            string currentColor = cardComponent.color[0];
+
+            // flip the color and add a phase change
+            switch (currentColor)
+            {
+                case "red":
+                    cardComponent.color[0] = "blue"; //|0> to |1>
+                    break;
+                case "blue":
+                    cardComponent.color[0] = "yellow"; // |1> to -|0>
+                    break;
+                case "yellow":
+                    cardComponent.color[0] = "green"; // -|0> to -|1>
+                    break;
+                case "green":
+                    cardComponent.color[0] = "red"; // -|1> to |0>
+                    break;
+            }
+
+            Debug.Log($"Y Gate applied! Color changed from {currentColor} to {cardComponent.color[0]}");
+
+        }
+        else
+        {
+            Debug.LogWarning("Top card in discard pile has no valid color.");
+        }
     }
-
-
 }
